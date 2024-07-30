9c6ffdb457dd6ca3acf16da2715e03db0c625788
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-07-30 17:24:02 +0530

    Creating User Model

---
+++ backend/model/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+    minLength: [4, "Name should have more than 4 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "please enter an Email"],
+    unique: [true, "Email already exist"],
+    validate: [validator.isEmail, "Invalid Email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please create password"],
+    select: false,
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
