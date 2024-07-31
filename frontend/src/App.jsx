3da8bd045f4de4337752d20ccfd8c9c4aedea2f3
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-07-31 10:04:41 +0530

    added Home Loader Login and scss mixins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import { lazy, Suspense } from "react";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Loader from "./components/Loader";
+
+const Login = lazy(() => import("./components/Login"));
+const Home = lazy(() => import("./components/Home"));
+
 const App = () => {
-  return <>hello</>;
+  return (
+    <Suspense fallback={<Loader />}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </Suspense>
+  );
 };
 
 export default App;
